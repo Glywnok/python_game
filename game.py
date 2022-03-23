87fb526185dbdf128a4ef1036ddeca5a383874b6
Glywnok <arturlohatin@gmail.com>
2022-03-23 10:47:03 +0200

    Update 0.2 Added settings and remade the main

--- game.py
+++ game.py
@@ -1,10 +1,12 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     #init game and create display object
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    game_settings = Settings()
+    screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height ))
     pygame.display.set_caption("Example Game, from the teacher")
 
     while True:
@@ -12,6 +14,8 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        #Background screen fill
+        screen.fill(game_settings.bg_color)
         #display the last screen
         pygame.display.flip()
 
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store all settings for example game."""
+
+    def __init__(self):
+        """Innitialize game settings"""
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (42, 181, 202)
