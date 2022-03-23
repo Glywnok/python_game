424ff4cec970b27cd73cbb66dcd7c9eaf51a9fd0
Glywnok <arturlohatin@gmail.com>
2022-03-23 13:04:37 +0200

    0.6 Added movement to left and right, only work if pressing once

--- game_functions.py
+++ game_functions.py
@@ -1,10 +1,19 @@
 import sys
 import pygame
-def check_events():
+def check_events(ship):
     """check keyboard snd mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.rect.centerx += 1
+            if event.key == pygame.K_LEFT:
+                ship.rect.centerx -= 1
+'''           if event.type == pygame.K_UP:
+                ship.rect.bottom -= 1
+            if event.type == pygame.K_DOWN:
+                ship.rect.bottom += 1       '''
 
 def update_screen(game_settings, screen, ship):
     """Update image on screen and draw new screen, eg frame after frame"""
